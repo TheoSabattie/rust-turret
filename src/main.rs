e5fa15a4b610f2f2deb01de1ca424a804d59575c
Théo Sabattié <theo.sabattie@gmail.com>
2021-07-25 01:43:39 +0200

    Add enemies at random position and rotation

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,11 @@ struct Resources {
 
 impl Resources {
     fn new(ctx:&mut Context) -> Self{
-        let mut enemy_pos = vec![Vec2::default(); 3];
-        let angle = std::f32::consts::PI*2.0/3.0;
-
-        for i in 0..3 {
-            enemy_pos[i] = Vec2::new((angle * i as f32).cos(), (angle * i as f32).sin());
-        }
+        let enemy_pos = vec![
+            Vec2::new(-30.0,  30.0), 
+            Vec2::new(-30.0,  -30.0),
+            Vec2::new(60.0,  0.0)
+        ];
 
         Self {
             player:Mesh::new_rectangle(ctx, DrawMode::Fill(FillOptions::default()), Rect::new(-15.0, -15.0, 100.0, 30.0), Color::RED).unwrap(),
@@ -26,6 +25,7 @@ impl Resources {
     }
 }
 
+#[derive(Clone)]
 struct Transform {
     position:Vec2,
     scale:Vec2,
@@ -57,20 +57,21 @@ fn main() {
     event::run(ctx, event_loop, my_game);
 }
 
-struct Shot {
+#[derive(Clone)]
+struct Mobile {
     transform:Transform,
     velocity:Vec2,
 }
 
-impl Shot {
+impl Mobile {
     fn do_action(&mut self){
         self.transform.position += self.velocity;
     }
 }
 
-impl Default for Shot {
+impl Default for Mobile {
     fn default() -> Self {
-        Self {
+        Mobile {
             transform:Transform::default(),
             velocity:Vec2::default()
         }
@@ -102,7 +103,7 @@ trait Graphic {
     fn get_transform(&self) -> &Transform;
 }
 
-impl Graphic for Shot {
+impl Graphic for Mobile {
     fn get_transform(&self) -> &Transform {
         &self.transform
     }
@@ -122,11 +123,12 @@ impl Player {
     }
 }
 
-
+const N_ENEMY:usize = 50;
 
 struct MyGame{
     player:Player,
-    shots:Vec<Shot>,
+    shots:Vec<Mobile>,
+    enemies:Vec<Mobile>,
     resources:Resources
 }
 
@@ -137,15 +139,28 @@ impl MyGame {
         let mut player = Player::default();
         player.transform.position = Vec2::new(width / 2.0, height / 2.0);
 
+        let mut enemies:Vec<Mobile> = vec![Mobile::default();N_ENEMY];
+
+        for i in 0..N_ENEMY {
+            let rotation_rand:f32 = rand::random();
+            let width_rand:f32    = rand::random();
+            let height_rand:f32   = rand::random();
+            let rotation  = (rotation_rand * 4.0).floor() * std::f32::consts::PI / 2.0 + std::f32::consts::PI /4.0;
+            enemies[i].transform.position = Vec2::new(width_rand * width, height * height_rand);
+            enemies[i].transform.rotation = rotation;
+            enemies[i].velocity = Vec2::new(rotation.cos() * 10.0, rotation.sin() * 10.0);
+        }
+
         Self {
             player:player,
             shots:Vec::new(),
+            enemies:enemies,
             resources:Resources::new(_ctx)
         }
     }
 
     fn player_shoot(&mut self){
-        let mut shot = Shot::default();
+        let mut shot = Mobile::default();
         let player_transform = &self.player.transform;
         let direction = Vec2::new(player_transform.rotation.cos(), player_transform.rotation.sin());
         shot.transform.position = player_transform.position + direction * 100.0;
@@ -163,17 +178,23 @@ impl EventHandler<ggez::GameError> for MyGame {
             self.shots[i].do_action();
         }
 
+        for i in 0..self.enemies.len() {
+            self.enemies[i].do_action();
+        }
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, Color::WHITE);
-        // Draw code here...
-
         graphics::draw(ctx, &self.resources.player, self.player.get_draw_params())?;
         
         for i in 0..self.shots.len() {
             graphics::draw(ctx, &self.resources.shot, self.shots[i].get_draw_params())?;
+        }
+
+        for i in 0..self.enemies.len() {
+            graphics::draw(ctx, &self.resources.enemy, self.enemies[i].get_draw_params())?;
         }
 
         graphics::present(ctx)
