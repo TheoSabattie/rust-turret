5c680a2e605dab6de93fe4c72dd67357cef0fd82
Théo Sabattié <theo.sabattie@gmail.com>
2021-07-26 01:03:31 +0200

    Add collision

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ggez::event::{self, EventHandler, MouseButton};
 use glam::*;
 
 struct Resources {
+    shield:Mesh,
     player:Mesh,
     shot:Mesh,
     enemy:Mesh
@@ -18,6 +19,7 @@ impl Resources {
         ];
 
         Self {
+            shield:Mesh::new_circle(ctx, DrawMode::Fill(FillOptions::default()), Vec2::default(), SHIELD_RADIUS, 1.0, Color::new(0.5, 0.5, 0.5, 1.0)).unwrap(),
             player:Mesh::new_rectangle(ctx, DrawMode::Fill(FillOptions::default()), Rect::new(-15.0, -15.0, 100.0, 30.0), Color::RED).unwrap(),
             shot:Mesh::new_circle(ctx, DrawMode::Fill(FillOptions::default()), Vec2::default(), 15.0, 1.0, Color::YELLOW).unwrap(),
             enemy:Mesh::new_polygon(ctx, DrawMode::Fill(FillOptions::default()), &enemy_pos, Color::GREEN).unwrap()
@@ -30,6 +32,11 @@ struct Transform {
     position:Vec2,
     scale:Vec2,
     rotation:f32,
+}
+
+struct Shield {
+    transform:Transform,
+    radius:f32,
 }
 
 impl Default for Transform {
@@ -70,14 +77,15 @@ struct Enemy {
 }
 
 impl Mobile {
-    fn do_action(&mut self){
+    fn do_action(&mut self, size:&Vec2) -> bool {
         move_with_velocity(&mut self.transform.position, &self.velocity);
-    }
-}
+        let position = &self.transform.position;
 
-impl Graphic for Enemy {
-    fn get_transform(&self) -> &Transform {
-        &self.transform
+        if position.x < 0.0 || position.x > size.x || position.y < 0.0 || position.y > size.y {
+            return true;
+        }
+
+        false
     }
 }
 
@@ -91,7 +99,7 @@ impl Default for Enemy {
 }
 
 impl Enemy {
-    fn do_action(&mut self, size:&Vec2){
+    fn do_action(&mut self, size:&Vec2, player_shield:&Shield){
         move_with_velocity(&mut self.transform.position, &self.velocity);
 
         let position = &self.transform.position;
@@ -113,10 +121,21 @@ impl Enemy {
             velocity_changed = true;
         }
 
+        let shield_to_self = Vec2::new(player_shield.transform.position.x - position.x, player_shield.transform.position.y - position.y);
+
+        if get_length(&shield_to_self) <= player_shield.radius {
+            self.velocity *= -1.0;
+            velocity_changed = true;
+        }
+
         if velocity_changed {
             self.transform.rotation = self.velocity.y.atan2(self.velocity.x);
         }
     }
+}
+
+fn get_length(vec:&Vec2) -> f32 {
+    (vec.x * vec.x + vec.y * vec.y).sqrt()
 }
 
 fn move_with_velocity(position:&mut Vec2, velocity:&Vec2){
@@ -145,29 +164,11 @@ impl Default for Player {
     }
 }
 
-trait Graphic {
-    fn get_draw_params(&self) -> DrawParam {
-        let transform = self.get_transform();
-
-        DrawParam::default()
-            .scale(transform.scale)
-            .dest(transform.position)
-            .rotation(transform.rotation)
-    }
-
-    fn get_transform(&self) -> &Transform;
-}
-
-impl Graphic for Mobile {
-    fn get_transform(&self) -> &Transform {
-        &self.transform
-    }
-}
-
-impl Graphic for Player {
-    fn get_transform(&self) -> &Transform {
-        &self.transform
-    }
+fn get_draw_params(transform:&Transform) -> DrawParam {
+    DrawParam::default()
+        .scale(transform.scale)
+        .dest(transform.position)
+        .rotation(transform.rotation)
 }
 
 impl Player {
@@ -179,8 +180,10 @@ impl Player {
 }
 
 const N_ENEMY:usize = 50;
+const SHIELD_RADIUS:f32 = 50.0;
 
-struct Game{
+struct Game {
+    shield:Shield,
     player:Player,
     shots:Vec<Mobile>,
     enemies:Vec<Enemy>,
@@ -190,30 +193,37 @@ struct Game{
 
 impl Game {
     pub fn new(_ctx: &mut Context) -> Self {
-        // Load/create resources such as images here.
         let (width, height) = graphics::drawable_size(_ctx);
         let mut player = Player::default();
-        player.transform.position = Vec2::new(width / 2.0, height / 2.0);
+        let size = Vec2::new(width, height);
+        let center = size / 2.0;
+        player.transform.position = center;
+        let size = Vec2::new(width, height);
 
-        let mut enemies:Vec<Enemy> = vec![Enemy::default();N_ENEMY];
+        Game {
+            shield:Shield{transform:player.transform.clone(), radius:SHIELD_RADIUS},
+            player:player,
+            shots:Vec::new(),
+            enemies:Self::create_enemies(&size, N_ENEMY),
+            resources:Resources::new(_ctx),
+            size:Vec2::new(width, height)
+        }
+    }
 
-        for i in 0..N_ENEMY {
+    fn create_enemies(size:&Vec2, n_enemy:usize) -> Vec<Enemy> {
+        let mut enemies:Vec<Enemy> = vec![Enemy::default();n_enemy];
+
+        for i in 0..n_enemy {
             let rotation_rand:f32 = rand::random();
             let width_rand:f32    = rand::random();
             let height_rand:f32   = rand::random();
-            let rotation  = (rotation_rand * 4.0).floor() * std::f32::consts::PI / 2.0 + std::f32::consts::PI /4.0;
-            enemies[i].transform.position = Vec2::new(width_rand * width, height * height_rand);
+            let rotation          = (rotation_rand * 4.0).floor() * std::f32::consts::PI / 2.0 + std::f32::consts::PI /4.0;
+            enemies[i].transform.position = Vec2::new(width_rand * size.x, size.y * height_rand);
             enemies[i].transform.rotation = rotation;
             enemies[i].velocity = Vec2::new(rotation.cos() * 10.0, rotation.sin() * 10.0);
         }
 
-        Game {
-            player:player,
-            shots:Vec::new(),
-            enemies:enemies,
-            resources:Resources::new(_ctx),
-            size:Vec2::new(width, height)
-        }
+        enemies
     }
 
     fn player_shoot(&mut self){
@@ -231,12 +241,29 @@ impl EventHandler<ggez::GameError> for Game {
         // Update code here...
         self.player.do_action(&*_ctx);
 
-        for i in 0..self.shots.len() {
-            self.shots[i].do_action();
+        for i in (0..self.shots.len()).rev() {
+            if self.shots[i].do_action(&self.size) {
+                self.shots.remove(i);
+            }
         }
 
         for i in 0..self.enemies.len() {
-            self.enemies[i].do_action(&self.size);
+            self.enemies[i].do_action(&self.size, &self.shield);
+        }
+
+        for i in (0..self.shots.len()).rev() {
+            let shot = &self.shots[i];
+
+            for j in (0..self.enemies.len()).rev() {
+                let enemy = &self.enemies[j];
+                let shot_to_enemy = shot.transform.position - enemy.transform.position;
+
+                if get_length(&shot_to_enemy) <= 40.0 {
+                    self.shots.remove(i);
+                    self.enemies.remove(j);
+                    break;
+                }
+            }
         }
 
         Ok(())
@@ -245,15 +272,17 @@ impl EventHandler<ggez::GameError> for Game {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, Color::WHITE);
         
+        graphics::draw(ctx, &self.resources.shield, get_draw_params(&self.shield.transform))?;
+
         for i in 0..self.enemies.len() {
-            graphics::draw(ctx, &self.resources.enemy, self.enemies[i].get_draw_params())?;
+            graphics::draw(ctx, &self.resources.enemy, get_draw_params(&self.enemies[i].transform))?;
         }
 
         for i in 0..self.shots.len() {
-            graphics::draw(ctx, &self.resources.shot, self.shots[i].get_draw_params())?;
+            graphics::draw(ctx, &self.resources.shot, get_draw_params(&self.shots[i].transform))?;
         }
 
-        graphics::draw(ctx, &self.resources.player, self.player.get_draw_params())?;
+        graphics::draw(ctx, &self.resources.player, get_draw_params(&self.player.transform))?;
         graphics::present(ctx)
     }
 
